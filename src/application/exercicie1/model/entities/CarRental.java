6adb69500fa674957058778913051912bf1562b7
Jairo Dias <jairopereira_dias@hotmail.com>
2021-09-04 16:06:11 -0300

    Learning interfaces

---
+++ src/application/exercicie1/model/entities/CarRental.java
@@ -0,0 +1,53 @@
+package application.exercicie1.model.entities;
+
+import java.util.Date;
+
+public class CarRental {
+    private Date start;
+    private Date finish;
+
+    private Vehicle vehicle;
+    private Invoice invoice;
+
+    public CarRental() {
+    }
+
+    public CarRental(Date start, Date finish, Vehicle vehicle) {
+        this.start = start;
+        this.finish = finish;
+        this.vehicle = vehicle;
+    }
+
+    public Date getStart() {
+        return this.start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getFinish() {
+        return this.finish;
+    }
+
+    public void setFinish(Date finish) {
+        this.finish = finish;
+    }
+
+    public Vehicle getVehicle() {
+        return this.vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Invoice getInvoice() {
+        return this.invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+}
---
+++ src/application/exercicie1/model/entities/Vehicle.java
@@ -0,0 +1,20 @@
+package application.exercicie1.model.entities;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
